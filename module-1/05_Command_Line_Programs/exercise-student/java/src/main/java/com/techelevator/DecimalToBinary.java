ade6119b3015cb6e1ed4f3a3473683fe2b110e9c
Bridgette Bergman <bridgette.bergman360@gmail.com>
2021-06-18 14:44:26 -0400

    Reuploading

--- module-1/05_Command_Line_Programs/exercise-student/java/src/main/java/com/techelevator/DecimalToBinary.java
+++ module-1/05_Command_Line_Programs/exercise-student/java/src/main/java/com/techelevator/DecimalToBinary.java
@@ -1,8 +1,28 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
+//Please enter in a series of decimal values (separated by spaces): 460 8218 1 31313 987654321
+//
+//		460 in binary is 111001100
+//		8218 in binary is 10000000011010
+//		1 in binary is 1
+//		31313 in binary is 111101001010001
+//		987654321 in binary is 111010110111100110100010110001
+
 public class DecimalToBinary {
 
 	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in); //initializes our object - constructor
+		System.out.println("Change a decimal to binary!");
+
+		System.out.println("Please enter in a series of decimal values (separated by spaces): ");
+		String input = scanner.nextLine();
+
+		String[] seriesArray = input
+//		double dec = Double.parseDouble(input);
+
 
 	}
 
