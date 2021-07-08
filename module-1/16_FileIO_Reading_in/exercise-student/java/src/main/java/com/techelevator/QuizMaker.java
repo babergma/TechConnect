2ebf61682272282eb780f4dd426b81fd84bb8c45
Bridgette Bergman <bridgette.bergman360@gmail.com>
2021-07-07 21:43:09 -0400

    Exceptions upload

--- module-1/16_FileIO_Reading_in/exercise-student/java/src/main/java/com/techelevator/QuizMaker.java
+++ module-1/16_FileIO_Reading_in/exercise-student/java/src/main/java/com/techelevator/QuizMaker.java
@@ -1,10 +1,83 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class QuizMaker {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws FileNotFoundException {
+		try (Scanner scan = new Scanner(System.in)) {
+
+			while (true) {
+				System.out.println("Insert name of quiz file: ");
+				String quizName = scan.nextLine();
+				File quizFile = new File(quizName);
+				if (quizFile.exists() == false) {
+					System.out.println("Error: " + quizName + "doesn't exist");
+				}
+				if (quizFile.isFile() == false) {
+					System.out.println("Error: " + quizName + "isnt a file");
+				}
+				Quiz(quizFile);
+			}
+		}
 
 	}
 
+	public static void Quiz(File quizFile) throws FileNotFoundException {
+		List<QuizQ> quizQ = new ArrayList<QuizQ>();
+		//scan file to get questions
+		Scanner fileScan = new Scanner(quizFile.getAbsoluteFile());
+
+		try {
+
+//					System.out.println("inside try");
+			while (fileScan.hasNextLine()) {
+
+				String lineFromTxt = fileScan.nextLine();
+				quizQ.add(new QuizQ(lineFromTxt));
+			}
+		}
+
+		catch (Exception e) {
+			System.out.println(e.getMessage());
+			System.exit(1);
+		}
+
+
+		int numQ = 0;
+		int numCorrectA = 0;
+
+		for(QuizQ quiz : quizQ) {
+
+			while(true) {
+
+				System.out.println("\n" + quiz.getQuestion());
+
+				for (int i = 0; i < quiz.getAnswers().length; i++) {
+					System.out.println((i + 1) + ") " + quiz.getAnswers()[i]);
+				}
+
+				System.out.println("\nYour answer?");
+				String userAnswer = fileScan.nextLine();
+
+				if ((userAnswer.isEmpty() == false) && (userAnswer.matches("[1-4]")) ) {
+					if (quiz.isCorrect(userAnswer)) {
+						System.out.println("Correct!");
+						numCorrectA += 1;
+					}
+					else {
+						System.out.println("Not correct!");
+					}
+					break;
+				}
+				System.out.println("Invalid answer!");
+			}
+			numQ += 1;
+		}
+		System.out.println("You got " + numCorrectA + " answers correct out of " + numQ);
+	}
 }
--- module-1/16_FileIO_Reading_in/exercise-student/java/src/main/java/com/techelevator/WordSearch.java
+++ module-1/16_FileIO_Reading_in/exercise-student/java/src/main/java/com/techelevator/WordSearch.java
@@ -1,9 +1,75 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class WordSearch {
 
-	public static void main(String[] args) {
 
+	public static void main(String[] args) {
+		Scanner scanUserInput = new Scanner(System.in); //takes in what user types
+		File file = null;
+		do{
+			System.out.println("Enter file name :");
+			try {
+				String fileName = scanUserInput.nextLine();
+				 file = new File(fileName);
+				System.out.println("File exists");
+
+//START: word search - get the word from the user that will be searched
+				System.out.println("What word would you like to search:");
+				String word = scanUserInput.nextLine(); //get the user input
+
+//START: case sensitive
+				System.out.println("Would you like the search to be case sensitive? Y or N");
+				boolean caseSensitive = scanUserInput.nextLine().toLowerCase().equals("y");
+
+				if(word != null || !word.isEmpty()) {
+					System.out.println("Success: the word is... " + word);
+
+//BEGIN: search for the word
+					int lineNumber = 1;
+					Scanner scan = new Scanner(file.getAbsoluteFile());
+//		System.out.println("In wordSearch: the word is... " + word);
+					try {
+						while (scan.hasNextLine()) {
+							String lineFromTxt = scan.nextLine();
+
+							if(caseSensitive) {
+
+								if (lineFromTxt.contains(word)) {
+									System.out.println(lineNumber + ") " + lineFromTxt);
+								}
+							}
+							else{
+								if (lineFromTxt.toLowerCase().contains(word.toLowerCase())) { // lower-case strings to make case-insensitive
+									System.out.println(lineNumber + ") " + lineFromTxt);
+								}
+							}
+	//END: case sensitive
+								lineNumber++;
+
+						}
+					}
+					catch (Exception e) {
+						System.out.println(e.getMessage());
+					}
+				}
+
+				else{
+					System.out.println("The search word is empty");
+					System.exit(1);
+				}
+
+//	END: search for the word
+
+
+			} catch (FileNotFoundException e) {
+				System.out.println("Error - file not found!");
+			}
+		}
+		while(!file.exists());
 	}
 
 }
